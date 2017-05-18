21d9d0b6e8b3d41c81f839eb7b68d0e3ff67b8ea
Mauricio Matsubara <mauricio@matsubara.co>
2017-05-18 19:25:58 -0300

    Initial Commit

---
+++ hw18may/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw18may
+//
+//  Created by Mauricio Matsubara on 18/05/17.
+//  Copyright © 2017 Mauricio Matsubara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
