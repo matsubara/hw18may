fe1d1060a0ec75d353ecd06f6e7ab86992585d5b
Breno Sallouti <brenosallouti@icloud.com>
2017-05-23 16:42:46 -0300

    O que está errado com isto?

---
+++ hw18may/InvestmentAccount.cpp
@@ -0,0 +1,9 @@
+//
+//  InvestmentAccount.cpp
+//  hw18may
+//
+//  Created by Breno Sallouti on 23/05/17.
+//  Copyright © 2017 Mauricio Matsubara. All rights reserved.
+//
+
+#include "InvestmentAccount.hpp"
---
+++ hw18may/InvestmentAccount.hpp
@@ -0,0 +1,14 @@
+//
+//  InvestmentAccount.hpp
+//  hw18may
+//
+//  Created by Breno Sallouti on 23/05/17.
+//  Copyright © 2017 Mauricio Matsubara. All rights reserved.
+//
+
+#ifndef InvestmentAccount_hpp
+#define InvestmentAccount_hpp
+
+#include <stdio.h>
+
+#endif /* InvestmentAccount_hpp */
